451058c7ca53bff0c2985092b17f400827e48ba8
Nadya Lenkovets <nadya.lenkovets@gmail.com>
2022-12-19 10:29:20 +0300

    Test commit 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { CounterFunc } from './components/Counter-func';
 import { CounterClass } from './components/Counter-class';
+import { Feature2 } from './components/Feature2';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className='header'>
         Git training
       </header>
+      <Feature2 />
       <div><CounterFunc /></div>
       <div><CounterClass /></div>
     </div>
